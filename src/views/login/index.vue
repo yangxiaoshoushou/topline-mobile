62214779a929f74935ac4ac1678aa70384b93227
yangping <2506186636@.qq.com>
2019-07-08 22:36:05 +0800

    登录页面布局完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,43 @@
 <template>
-<div>Login Component</div>
+  <div class="login-wrap">
+    <van-nav-bar title="标题" />
+    <form>
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          required
+          clearable
+          label="手机号"
+          placeholder="请输入手机号"
+        />
+        <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
+      </van-cell-group>
+      <div class="login-btn">
+        <van-button class="btn" type="info">登录</van-button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.login-btn {
+  padding: 10px;
+  .btn {
+    width: 100%;
+  }
+}
 </style>
