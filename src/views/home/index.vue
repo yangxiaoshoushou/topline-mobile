d75ad84d4210822da01069cd19344e13fef09b26
yangping <2506186636@.qq.com>
2019-07-08 22:09:49 +0800

    准备两个页面组件

---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Home Component</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIndex'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Login Component</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
