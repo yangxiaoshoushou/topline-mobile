f1b1d0e6f3857529e96833092375f8f656cc47b3
yangping <2506186636@.qq.com>
2019-07-08 23:00:04 +0800

    请求登录成功

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,23 +3,19 @@
     <van-nav-bar title="标题" />
     <form>
       <van-cell-group>
-        <van-field
-          v-model="user.mobile"
-          required
-          clearable
-          label="手机号"
-          placeholder="请输入手机号"
-        />
+        <van-field v-model="user.mobile" required clearable label="手机号" placeholder="请输入手机号" />
         <van-field v-model="user.code" type="password" label="密码" placeholder="请输入密码" required />
       </van-cell-group>
       <div class="login-btn">
-        <van-button class="btn" type="info">登录</van-button>
+        <van-button class="btn" type="info" @click.prevent="handleLogin">登录</van-button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'LoginIndex',
   data () {
@@ -28,6 +24,25 @@ export default {
         mobile: '',
         code: ''
       }
+    }
+  },
+  // methods () {
+  //   handleLogin () {
+  //     const res = await axios({
+  //       method: 'POST',
+  //       url: 'Http://toutiao.course.itcast.cn/app/v1_0/authorizations',
+  //       data: this.user
+  //     })
+  //   }
+  // }
+  methods: {
+    async  handleLogin () {
+      const res = await axios({
+        method: 'POST',
+        url: 'Http://toutiao.course.itcast.cn/app/v1_0/authorizations',
+        data: this.user
+      })
+      console.log(res)
     }
   }
 }
