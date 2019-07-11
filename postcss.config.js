3475ca2df7e1d1f9a1b433446a9fd24db1e7ce63
yangping <2506186636@.qq.com>
2019-07-11 20:34:18 +0800

    基于axios封装request请求模块

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,27 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*']
+const { sep } = require('path')
+
+module.exports = ({ file }) => {
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
     }
   }
 }
+
+// module.exports = {
+//   plugins: {
+//     autoprefixer: {},
+//     'postcss-pxtorem': {
+//       rootValue: 37.5,
+//       propList: ['*']
+//     }
+//   }
+// }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-<div>Home Component</div>
+<div>
+    <div class="box"></div>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+</div>
 </template>
 
 <script>
@@ -9,5 +16,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.box {
+  width: 375px;
+  height: 200px;
+  background-color: #f40;
+}
 </style>
