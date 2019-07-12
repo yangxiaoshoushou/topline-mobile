1bd0e960e03c157c7f04f05587d747e20ecba281
yangping <2506186636@.qq.com>
2019-07-12 13:38:07 +0800

    首页-布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,68 @@
 <template>
-<div>
-  <div class="home">
-    <van-nav-bar title="首页"/>
+  <div>
+    <div class="home">
+      <van-nav-bar title="首页" />
+      <van-tabs v-model="activeChannelIndex">
+          <van-tab title="标签 1">
+          <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+          <van-cell
+            v-for="item in list"
+            :key="item"
+            :title="item"
+          />
+          </van-list>
+          </van-pull-refresh>
+        </van-tab>
+        <van-tab title="标签 2">内容 2</van-tab>
+        <van-tab title="标签 3">内容 3</van-tab>
+        <van-tab title="标签 4">内容 4</van-tab>
+      </van-tabs>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIndex'
+  name: 'HomeIndex',
+  data () {
+    return {
+      activeChannelIndex: 0,
+      list: [],
+      loading: false,
+      finished: false,
+      isLoading: false
+    }
+  },
+
+  methods: {
+    onLoad () {
+    // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 1000)
+    },
+    onRefresh () {
+      console.log('onRefresh')
+      setTimeout(() => {
+        this.isLoading = false
+      }, 3000)
+    }
+  }
 }
 </script>
 
